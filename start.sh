d702e0857a6c188f04a7b983da3a2854e1664bbe
Sushii <jrmy.sp@gmail.com>
2024-01-03 18:53:56 -1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 cd /app/src
 git clone https://github.com/tesseract-ocr/tesstrain.git
 echo "Git clone done"
-mkdir -p /app/src/tesstrain/data/dws-ground-truth
 cd /app/src/tesstrain
+echo "Downloading ..."
 make tesseract-langdata
+mkdir -p /app/src/tesstrain/data/dws-ground-truth
 echo "Container ready"
 tail -f /dev/null
